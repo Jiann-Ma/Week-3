826a6385b170e22767a8a751771594f257cf4f28
Jiann <72090089+Jiann-Ma@users.noreply.github.com>
2021-03-10 20:30:11 +0800

    Update 要求一.py

--- 要求一.py
+++ 要求一.py
@@ -8,8 +8,28 @@ with request.urlopen(src) as response:
 travelSpot=data["result"]["results"]
 with open("data.txt", "w", encoding="utf-8") as file:
     for info in travelSpot:
-        file.write(info["stitle"]+","+info["longitude"]+","+info["latitude"]+","+(info["file"]).split(".jpg", 6)[0]+".jpg"+"\n")
+        file.write(info["stitle"]+","+info["longitude"]+","+info["latitude"]+","+("http"+(info["file"]).split("http")[1])+"\n")
      
 
 #景點名稱,經度,緯度,第一張圖檔網址
 #新北投溫泉區,123.5446,24.5312,http://www.travel.taipei/pic/11000848.jpg
+
+
+
+
+
+#下面是比較不好的寫法，因為只能抓到jpg結尾的
+#網路串接公開資料
+'''
+import urllib.request as request
+import json
+src="https://padax.github.io/taipei-day-trip-resources/taipei-attractions.json"
+with request.urlopen(src) as response:
+    data=json.load(response)
+#列出景點名稱
+travelSpot=data["result"]["results"]
+with open("data.txt", "w", encoding="utf-8") as file:
+    for info in travelSpot:
+        file.write(info["stitle"]+","+info["longitude"]+","+info["latitude"]+","+(info["file"]).split(".jpg", 6)[0]+".jpg"+"\n")
+'''
+
